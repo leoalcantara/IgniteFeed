bd63c522b4ccbf4171ab2f45ff7e1fc2809f8e2f
unknown <leonidio.ti@gmail.com>
2023-08-23 15:33:39 -0300

    feature: Sidebar component created

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine  } from '@phosphor-icons/react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}> 
+            <img
+              className={styles.cover} 
+              src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=58"  
+            />
+
+            <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/10419679?v=4"                     
+                />
+
+                <strong>Leonidio Alcantara</strong>
+                <span>Developer</span>
+            </div>
+            
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+        
+    );        
+}
