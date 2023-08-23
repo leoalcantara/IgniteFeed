6c1ee1a37c9e48a3ebed79ea5d00d42652b40fa1
unknown <leonidio.ti@gmail.com>
2023-08-22 22:56:21 -0300

    file global.css created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import { Header } from './components/Header'
-import './App.css'
+import { Header } from './components/Header';
+import './global.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>  
